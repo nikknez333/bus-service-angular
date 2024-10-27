c1baf422cd2740bda1b69f91fb446b98462dc0b8
nikknez333 <nebojsaknezz@gmail.com>
2024-10-27 11:28:24 +0000

    Initial HTML/CSS set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { TopHeaderComponent } from './header/top-header/top-header.component';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faUser} from '@fortawesome/free-solid-svg-icons';
+import { faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary){
+    library.addIcons(faUser, faTwitter);
+  }
+}
